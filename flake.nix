5ed3e5e09a4e42a1993a8cbc2092f62f2b6486dd
mikuri <navaljotap@gmail.com>
2026-02-10 20:30:04 -0600

    Update flake.nix configuration and inputs

--- flake.nix
+++ flake.nix
@@ -1,42 +1,77 @@
 {
-  description = "flake de  Mikuri";
+  description = "Configuración NixOS de Mikuri";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    noctalia-shell.url = "github:Noctalia-dev/noctalia-shell";
-    matugen.url = "github:InioX/Matugen/matugen-v0.10.0";
+#    oxicord.url = "github:linuxmobile/oxicord";
 
-    mikuboot = {
-      url = "gitlab:evysgarden/mikuboot";
-      inputs.nixpkgs.follows = "";
+    noctalia-shell = {
+      url = "github:Noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     quickshell = {
       url = "github:outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  
+
+ #   nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel";
+
+    mikuboot = {
+      url = "gitlab:evysgarden/mikuboot";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+#    slowfetch = {
+#      url = "github:tuibird/Slowfetch";
+#      inputs.nixpkgs.follows = "nixpkgs";
+#    };
+
     silentSDDM = {
       url = "github:uiriansan/SilentSDDM";
       inputs.nixpkgs.follows = "nixpkgs";
     };
- 
+
+    elyprismlauncher.url = "github:ElyPrismLauncher/ElyPrismLauncher/10.0.2";
+
+    mango = {
+      url = "github:DreamMaoMao/mango";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, matugen, mikuboot, quickshell, noctalia-shell, silentSDDM, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
- 
-      specialArgs = {
-        inherit inputs;
-       };  
-     
-       modules = [
-        ./configuration.nix
-        mikuboot.nixosModules.default 
+      specialArgs = { inherit inputs; };
+
+      modules = [
         {
-          environment.systemPackages = [
-            noctalia-shell.packages.x86_64-linux.default
+          nixpkgs.config.allowUnfree = true;
+      #    nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
+        }
+
+        inputs.mikuboot.nixosModules.default
+        inputs.mango.nixosModules.mango
+
+        ./configuration.nix
+        ./sddm.nix
+        ./kernel.nix
+        ./servicio.nix
+        ./portal.nix
+        ./pkgs.nix
+
+        {
+          programs.mango.enable = true;
+        }
+
+        {
+          environment.systemPackages = with nixpkgs.legacyPackages.x86_64-linux; [
+
+            inputs.noctalia-shell.packages.x86_64-linux.default
+            inputs.elyprismlauncher.packages.x86_64-linux.default
+            inputs.quickshell.packages.x86_64-linux.default
+ #           inputs.slowfetch.packages.x86_64-linux.default
           ];
         }
       ];
